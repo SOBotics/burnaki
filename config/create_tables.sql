ef9ab3b5d20f92c9c1abfca8b1e1cdfb8b06062e
Tunaki <guillaume_boue@orange.fr>
2016-08-28 01:15:56 +0200

    latest schema

--- config/create_tables.sql
+++ config/create_tables.sql
@@ -19,6 +19,7 @@ CREATE TABLE IF NOT EXISTS `burnaki`.`burnination` (
   `tag` VARCHAR(35) CHARACTER SET 'utf8' NOT NULL,
   `start_date` DATETIME NOT NULL,
   `end_date` DATETIME NULL DEFAULT NULL,
+  `last_refresh_date` DATETIME NULL DEFAULT NULL,
   `meta_link` VARCHAR(255) CHARACTER SET 'utf8' NOT NULL,
   `room_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
@@ -42,6 +43,7 @@ CREATE TABLE IF NOT EXISTS `burnaki`.`burnination_progress` (
   `roombad` INT(11) NOT NULL,
   `manually_deleted` INT(11) NOT NULL,
   `retagged` INT(11) NOT NULL,
+  `opened_with_tag` INT(11) NOT NULL,
   PRIMARY KEY (`burnination_id`, `progress_date`),
   INDEX `fk_burnination_progress_burnination_idx` (`burnination_id` ASC),
   CONSTRAINT `fk_burnination_progress_burnination`
@@ -62,12 +64,27 @@ DROP TABLE IF EXISTS `burnaki`.`burnination_question` ;
 CREATE TABLE IF NOT EXISTS `burnaki`.`burnination_question` (
   `burnination_id` BIGINT(20) NOT NULL,
   `question_id` INT(11) NOT NULL,
+  `title` VARCHAR(255) CHARACTER SET 'utf8' NOT NULL,
+  `link` VARCHAR(255) CHARACTER SET 'utf8' NOT NULL,
+  `share_link` VARCHAR(255) CHARACTER SET 'utf8' NOT NULL,
+  `score` INT(11) NOT NULL,
+  `view_count` INT(11) NOT NULL,
+  `answer_count` INT(11) NOT NULL,
+  `comment_count` INT(11) NOT NULL,
+  `locked` TINYINT(1) NOT NULL,
+  `migrated` TINYINT(1) NOT NULL,
+  `answered` TINYINT(1) NOT NULL,
+  `accepted_answer_id` INT(11) NULL DEFAULT NULL,
+  `tags` VARCHAR(255) CHARACTER SET 'utf8' NOT NULL,
   `created_date` DATETIME NOT NULL,
+  `closed_date` DATETIME NULL DEFAULT NULL,
+  `closed_as_duplicate` TINYINT(1) NOT NULL,
+  `deleted_date` DATETIME NULL DEFAULT NULL,
+  `last_edit_date` DATETIME NULL DEFAULT NULL,
   `close_vote_count` INT(11) NOT NULL DEFAULT '0',
   `reopen_vote_count` INT(11) NOT NULL DEFAULT '0',
   `delete_vote_count` INT(11) NOT NULL DEFAULT '0',
   `undelete_vote_count` INT(11) NOT NULL DEFAULT '0',
-  `closed` TINYINT(1) NOT NULL DEFAULT '0',
   `roombad` TINYINT(1) NOT NULL DEFAULT '0',
   `manually_deleted` TINYINT(1) NOT NULL DEFAULT '0',
   `retagged` TINYINT(1) NOT NULL DEFAULT '0',
@@ -97,37 +114,6 @@ CREATE TABLE IF NOT EXISTS `burnaki`.`burnination_question_history` (
     FOREIGN KEY (`burnination_id` , `question_id`)
     REFERENCES `burnaki`.`burnination_question` (`burnination_id` , `question_id`)
     ON DELETE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
-
-
--- -----------------------------------------------------
--- Table `burnaki`.`se_api_question_cache`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `burnaki`.`se_api_question_cache` ;
-
-CREATE TABLE IF NOT EXISTS `burnaki`.`se_api_question_cache` (
-  `id` INT(11) NOT NULL,
-  `title` VARCHAR(250) CHARACTER SET 'utf8' NOT NULL,
-  `share_link` VARCHAR(255) CHARACTER SET 'utf8' NOT NULL,
-  `deleted_date` DATETIME NULL DEFAULT NULL,
-  `created_date` DATETIME NOT NULL,
-  `score` INT(11) NOT NULL,
-  `answer_count` INT(11) NOT NULL,
-  `locked` TINYINT(1) NOT NULL,
-  `migrated` TINYINT(1) NOT NULL,
-  `view_count` INT(11) NOT NULL,
-  `comment_count` INT(11) NOT NULL,
-  `closed_date` DATETIME NULL DEFAULT NULL,
-  `closed_as_duplicate` TINYINT(1) NOT NULL,
-  `answered` TINYINT(1) NOT NULL,
-  `with_accepted_answer` TINYINT(1) NOT NULL,
-  `close_vote_count` INT(11) NOT NULL,
-  `reopen_vote_count` INT(11) NOT NULL,
-  `tags` VARCHAR(255) CHARACTER SET 'utf8' NOT NULL,
-  `last_edit_date` DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_unicode_ci;
